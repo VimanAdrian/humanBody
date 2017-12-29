0668949b34dc63c1e35193bcc34f8c68fcd6d0c2
Viman Adrian <a.viman@yahoo.com>
2017-12-29 20:18:06 +0200

    added icon

--- src/main/java/controller/Main.java
+++ src/main/java/controller/Main.java
@@ -3,10 +3,14 @@ package controller;
 import javafx.application.Application;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.scene.input.MouseButton;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
+
+import java.io.File;
+import java.net.URLDecoder;
 
 public class Main extends Application {
 
@@ -30,6 +34,25 @@ public class Main extends Application {
 
                     }
                 });
+
+        //set icons
+        try {
+            String path = "";
+            String decodedPath = "";
+            path = Main.class.getProtectionDomain().getCodeSource().getLocation().getPath();
+            decodedPath = URLDecoder.decode(path, "UTF-8");
+            decodedPath = decodedPath.substring(1, decodedPath.lastIndexOf("/") + 1);
+            File tmp1 = new File(decodedPath + "resources/brain16.png");
+            File tmp2 = new File(decodedPath + "resources/brain24.png");
+            File tmp3 = new File(decodedPath + "resources/brain32.png");
+            File tmp4 = new File(decodedPath + "resources/brain256.png");
+            stage.getIcons().add(new Image(tmp1.toURI().toString()));
+            stage.getIcons().add(new Image(tmp2.toURI().toString()));
+            stage.getIcons().add(new Image(tmp3.toURI().toString()));
+            stage.getIcons().add(new Image(tmp4.toURI().toString()));
+        }catch (Exception e){
+            System.out.println("no icon");
+        }
         // show stage
         stage.show();
     }
